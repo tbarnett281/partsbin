b0ddedbd7ffadefe01ba15c53096f8a83136fea2
tbarnett281 <tbarnett281@gmail.com>
2022-04-01 15:46:55 -0500

    added nav bar

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './MainNavigation.css';
+import MainHeader from './MainHeader';
+import { Link } from 'react-router-dom';
 
-const MainNavigation = props => {};
+const MainNavigation = props => {
+    return <MainHeader>
+        <button className='main-navigation__menu-btn'>
+            <span />
+            <span />
+            <span />
+        </button>
+        <h1 className='main-navigation__title'>
+            <Link to="/">Parts Bin</Link>
+        </h1>
+        <nav>
+            ...
+        </nav>
+    </MainHeader>
+};
 
 export default MainNavigation;
